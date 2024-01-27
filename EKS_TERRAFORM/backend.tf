61e208a95614706b2fc7797819e24410b25e46a1
fdjapi <122319918+fdjapi@users.noreply.github.com>
2024-01-27 14:49:15 -0500

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tests3bucket1975" # Replace with your actual S3 bucket name
+    bucket = "test-bucket01161975" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
